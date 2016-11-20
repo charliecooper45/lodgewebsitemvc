59e39055ccb9bf738e497dd7c669444d63d05947
Charlie Cooper <charliecooper98@gmail.com>
2016-11-20 13:12:46 +0000

    minor update to resource url in security config

--- lodgewebsitemvc-core/src/main/java/uk/cooperca/lodge/website/mvc/config/SecurityConfig.java
+++ lodgewebsitemvc-core/src/main/java/uk/cooperca/lodge/website/mvc/config/SecurityConfig.java
@@ -78,7 +78,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources**");
+        web.ignoring().antMatchers("/resources/**");
     }
 
     @Bean
